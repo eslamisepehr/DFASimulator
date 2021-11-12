0f5d582112e9ad2fdf9c977f659c25092ee8fc3f
EslamiSepehr <eslamisepehr@gmail.com>
2021-11-12 14:48:54 +0330

    Add project files.

---
+++ src/DFASimulator.ConsoleApp/DFA.cs
@@ -0,0 +1,88 @@
+﻿namespace DFASimulator.ConsoleApp
+{
+    class DFA
+    {
+
+        private State StartState { get; set; }
+        private List<State> FinalStates { get; set; }
+        private List<State> States { get; set; }
+
+        public DFA()
+        {
+            FinalStates = new List<State>();
+            States = new List<State>();
+        }
+
+        public DFA AddStartState(State startState)
+        {
+            StartState = startState;
+            return this;
+        }
+
+        public DFA AddFinalState(State finalState)
+        {
+            FinalStates.Add(finalState);
+            return this;
+        }
+
+        public DFA AddState(State state)
+        {
+            States.Add(state);
+            return this;
+        }
+        public DFA AddState(List<State> states)
+        {
+            States.AddRange(states);
+            return this;
+        }
+
+        public bool Run(string input)
+        {
+            var currentState = StartState;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentState = currentState.GetDestination(input[i].ToString());
+            }
+
+            return FinalStates.Contains(currentState);
+        }
+
+    }
+
+
+    class State
+    {
+        public State(string name)
+        {
+            Name = name;
+            Transitions = new Dictionary<string, State>();
+        }
+
+        private string Name { get; set; }
+        private Dictionary<string, State> Transitions { get; set; }
+
+
+        public void AddRoute(string transition, State destination)
+        {
+            Transitions.Add(transition, destination);
+        }
+
+        public State GetDestination(string transition)
+        {
+            var isValid = Transitions.TryGetValue(transition, out var destination);
+            if (!isValid)
+            {
+                throw new Exception("State is not valid!");
+            }
+
+            return destination;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ src/DFASimulator.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using DFASimulator.ConsoleApp;
+
+var A = new State("A");
+var B = new State("B");
+var C = new State("C");
+var D = new State("D");
+
+A.AddRoute("1", A);
+A.AddRoute("0", B);
+B.AddRoute("1", A);
+B.AddRoute("0", C);
+C.AddRoute("1", A);
+C.AddRoute("0", D);
+D.AddRoute("0", D);
+D.AddRoute("1", D);
+
+
+Console.Write("Please enter the string: ");
+var input = Console.ReadLine();
+
+var dfa = new DFA()
+    .AddState(A).AddState(B).AddState(C).AddState(D)
+    .AddStartState(A)
+    .AddFinalState(A);
+
+var result = dfa.Run(input);
+Console.WriteLine($"{input} is valid? {result}");
